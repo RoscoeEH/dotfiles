e2e48122d3b18f67ad59ca1ceae6d459c23813aa
Roscoe Elings-Haynie <elingshaynier@gmail.com>
2026-01-31 21:37:16 -0800

    airplane mode toggle only impacts wifi

--- tools/Tools/airplane-mode.sh
+++ tools/Tools/airplane-mode.sh
@@ -1,20 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 wifi_state=$(nmcli -t -f WIFI radio)
 
-bt_raw=$(bluetoothctl show 2>/dev/null | awk -F': ' '/Powered:/ {print tolower($2); exit}')
-if [[ "$bt_raw" == "yes" ]]; then
-    bt_state="enabled"
-elif [[ "$bt_raw" == "no" ]]; then
-    bt_state="disabled"
-else
-    bt_state="unknown"
-fi
-
-if [[ "$wifi_state" == "disabled" || "$bt_state" == "disabled" ]]; then
+if [ "$wifi_state" == "disabled" ]; then
     nmcli radio all on
-    bluetoothctl power on >/dev/null
 else
     nmcli radio all off
-    bluetoothctl power off >/dev/null
 fi
