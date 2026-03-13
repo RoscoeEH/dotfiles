7bea32c0bc316ac54ac5c35306fd20829fb08486
Roscoe Elings-Haynie <elingshaynier@gmail.com>
2026-03-13 16:45:37 -0700

    fixed selection screenshot bug

--- tools/Tools/screenshots/screenshot-selection.sh
+++ tools/Tools/screenshots/screenshot-selection.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-grim -g "$(slurp)" - | tee ~/Pictures/Screenshots/screenshot_$(date +%F_%T).png | wl-copy
+file=~/Pictures/Screenshots/screenshot_$(date +%F_%T).png
+if grim -g "$(slurp)" "$file" && [ -s "$file" ]; then
+    wl-copy < "$file"
+else
+    rm -f "$file"
+fi
