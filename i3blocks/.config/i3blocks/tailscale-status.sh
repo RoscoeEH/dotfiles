a9eaa3d0bd5fd4411e2d3addb132e2d487d4b4f0
RoscoeEH <elingshaynier@gmail.com>
2026-05-27 18:06:30 -0400

    updated ts status to show errors

--- i3blocks/.config/i3blocks/tailscale-status.sh
+++ i3blocks/.config/i3blocks/tailscale-status.sh
@@ -4,9 +4,13 @@ if tailscale status | grep -q "Tailscale is stopped."; then
     echo "DOWN"
     echo ""
     echo "#5a8ffa"
-else
+elif tailscale status | grep -q "roscoe-btq-laptop"; then
     echo "UP"
     echo ""
     echo "#a442f5"
+else
+    echo "ERROR"
+    echo ""
+    echo "#ff0000"
 fi
 
